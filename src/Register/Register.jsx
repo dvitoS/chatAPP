06f7b40f2904b133a4a93e24cd79b57716b7c8f2
dvitoS <vito.skender@gmail.com>
2023-04-21 16:23:57 +0200

    "fixed firestore create user"

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,87 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "../Assets/avatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { auth, db, storage } from "../firebase";
-import { doc, setDoc } from "firebase/firestore"; 
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import './Register.css';
-import Avatar from '../Assets/avatar.png';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
+const Register = () => {
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-const Register = () =>{
-    const[err,setErr] = useState(false);
-    const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
 
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
 
-        try{
-            const res = await createUserWithEmailAndPassword(auth, email, password);           
-            const storageRef = ref(storage, displayName);
-            const uploadTask = await uploadBytesResumable(storageRef, file);
-   
-            uploadTask.on(               
-              (error) => {
-                setErr(true);          
-              }, 
-              () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-                        await updateProfile(res.user, {
-                            displayName,
-                            photoURL: downloadURL,
-                        });
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
+    }
+  };
 
-                        await setDoc(doc(db, "users", res.user.uid), {
-                            uid: res.user.uid,
-                            displayName,
-                            email,
-                            photoURL: downloadURL,
-                          });
-
-                        await setDoc(doc(db, "userChats", res.user.uid),{});
-                        navigate("/");                         
-                    });
-                }
-            );
-              
-        } catch(err) {  
-          setErr(true);
-        }
-    };
-
-    return(
-        <div className="formContainer">
-            <div className="formWrapper">
-                <span className="logo">VeryApp</span>
-                <span className="title">Register</span>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="display name" />
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="password" />
-                    <input style={{display:"none"}} type="file" id="file" />
-                    <label htmlFor="file">
-                        <img src={Avatar} alt="" />
-                        <p>Add an avatar</p>
-                    </label>
-                    <button>Sign up</button>
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>Already have an account?
-                    <Link to="/login">
-                        Log in here
-                    </Link>
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">VeryApp</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Avatar} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You already have an account? <Link to="/register">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
